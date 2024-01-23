f8c2c7e3879c3e0427549b97bdad5b706bd74502
evan nurandiz <muhammad.nurandiz@jubelio.com>
2024-01-23 11:25:13 +0700

    fix bug

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -887,86 +887,12 @@
         <!--Brand One End-->
 
         <!--Contact One Start-->
-        <section class="contact-one">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-4 col-lg-5">
-                        <div class="contact-one__left">
-                            <div class="section-title text-left">
-                                <span class="section-title__tagline">contact now</span>
-                                <h2 class="section-title__title">Get in touch with us</h2>
-                            </div>
-                            <div class="contact-one__person" style="margin-bottom: 60px;">
-                                <div class="contact-one__person-content">
-                                    <img src="{{asset('/assets/images/contact-image-1.svg')}}" alt="">
-                                </div>
-                            </div>
-                            <!-- <p class="contact-one__text-1">Lorem ipsum dolor sit amet, consecte adipiscing elit. In hac
-                                habitasse platea dictumst. Duis porta, quam ut finibus ultrices.</p> -->
-                            <ul class="list-unstyled contact-one__list">
-                                <li>
-                                    <div class="icon">
-                                        <i class="fas fa-phone-alt"></i>
-                                    </div>
-                                    <div class="content">
-                                        <p>Have any question?</p>
-                                        <h4> <span>Free</span> <a href="tel:0818830153">0818 830 153</a></h4>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="icon">
-                                        <i class="fas fa-envelope"></i>
-                                    </div>
-                                    <div class="content">
-                                        <p>Write email</p>
-                                        <h4><a href="mailto:needhelp@company.com">WWW.PURINUSAKEMBANGAN.COM</a></h4>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-xl-8 col-lg-7">
-                        <div class="contact-one__right">
-                            <div class="row">
-                                <div class="contact-one__form-box">
-                                    <form action="assets/inc/sendemail.php"
-                                        class="contact-one__form contact-one-validated" novalidate="novalidate">
-                                        <div class="row">
-                                            <div class="col-xl-6 col-lg-6 col-md-6">
-                                                <div class="contact-one__input-box">
-                                                    <input type="text" placeholder="Your name" name="name">
-                                                </div>
-                                            </div>
-                                            <div class="col-xl-6 col-lg-6 col-md-6">
-                                                <div class="contact-one__input-box">
-                                                    <input type="email" placeholder="Email address" name="email">
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-xl-12">
-                                                <div class="contact-one__input-box text-message-box">
-                                                    <textarea name="message" placeholder="Write message"></textarea>
-                                                </div>
-                                                <div class="contact-one__btn-box">
-                                                    <a href="https://api.whatsapp.com/send?phone=6281317876888&text=Hi, Saya ingin menjadwalkan visit." class="thm-btn contact-one__btn">Send a
-                                                        Message</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+        
         <!--Contact One End-->
 
         <!--News One Start-->
         <section class="news-one">
-            <div class="container">
+            <div class="container" style="margin-top: 240px; margin-bottom: 240px">
                 <div class="section-title text-center">
                     <span class="section-title__tagline">What’s Happening</span>
                     <h2 class="section-title__title">Latest news updates <br> & articles</h2>
